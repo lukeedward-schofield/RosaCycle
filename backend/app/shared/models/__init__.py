89bec9b15395311b15b9ceddf7bf30beb73a3de7
Luke Edward Schofield <schofieldluke14@gmail.com>
2026-08-06 14:21:55 +0800

    feat/messaging: added the messaging to routes and __init__.py

--- backend/app/shared/models/__init__.py
+++ backend/app/shared/models/__init__.py
@@ -3,8 +3,7 @@ from app.auth.user_model import User  # noqa: F401
 from app.trades.model import Trade  # noqa: F401
 from app.trades.offer.model import Offer  # noqa: F401
 
-from app.trades.messaging.model import Message
-from app.trades.messaging.model import Conversation
+from app.trades.messaging.model import Message, Conversation
 
 from app.map.model import ResourceSpot  # noqa: F401
 from app.shared.notification.model import Notification  # noqa: F401
